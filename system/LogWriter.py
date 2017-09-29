523e2d431c991b78b007fdedfe0136b02206d88c
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-09-29 22:50:58 +0700

    Remove `@statismethod`, call system variables and functions

--- system/LogWriter.py
+++ system/LogWriter.py
@@ -4,25 +4,25 @@ import global_var
 from k_date import date
 
 
-# Kaster' log file manipulator
+# Kaster' log file manipulator, deals everything that involves the log file
 class LogWriter(object):
     origin = os.getcwd()
 
-    @staticmethod
-    def create_log_file():
+    def create_log_file(self):
         try:
-            os.mkdir("/usr/share/kaster")
-            os.mknod("/usr/share/kaster/log.dat")
-            with open("/usr/share/kaster/log.dat", "w") as f:
+            os.mkdir(global_var.program_file_dir)
+            os.mknod(global_var.log_file_dir)
+            with open(global_var.log_file_dir, "w") as f:
                 f.write("Log file created on %s." % (date(kaster.date_format)))
+        except FileExistsError:
+            return
         except Exception as e:
             print(e)
             sys.exit(1)
 
-    @staticmethod
-    def write_to_log(log_str):
+    def write_to_log(self, log_str):
         try:
-            with open("/usr/share/kaster/log.dat", "w") as f:
+            with open(global_var.log_file_dir, "a") as f:
                 f.write(date(kaster.date_format) + log_str + "\n")
         except FileNotFoundError:
             create_log_file()
--- system/pre_kaster.py
+++ system/pre_kaster.py
@@ -2,8 +2,11 @@
 import sys
 import os
 import global_var
+from LogWriter import LogWriter
+
+k_log_writer = LogWriter()
 
 
 def check_program_file_dir():
     if not os.path.isdir(global_var.program_file_dir):
-        os.mkdir(global_var.program_file_dir)
+        k_log_writer.create_log_file()
