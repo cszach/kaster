a7c1a2455869c8ad8ffefc11b50275dd110b126e
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-09-29 22:49:50 +0700

    Add Docstrings

--- system/k_date.py
+++ system/k_date.py
@@ -4,8 +4,12 @@ months_name = ["January", "February", "March", "April", "May", "June",
                "July", "August", "September", "October", "November", "December"]
 
 
-# Return a date string aligned in the given format
 def date(d_format):
+    """
+    Return the date string, formatted by the format specified
+    :param d_format: The format to be applied to the date string
+    :return: The date string, formatted by the d_format
+    """
     now = datetime.now()
 
     f_format = d_format.upper().split("/")
