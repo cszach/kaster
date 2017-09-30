49dac488be402a5ac531bdff92155e36ea5e2d50
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-09-30 16:11:39 +0700

    Put files in src folder

---
+++ src/system/Instructor.py
@@ -0,0 +1,3 @@
+def print_help():
+    """Print the manual page of Kaster, which includes all arguments"""
+    print("Kaster Password Vault")
---
+++ src/system/k_random.py
@@ -0,0 +1,3 @@
+def random_string(*args):
+    if len(agrs) == 2:
+        
