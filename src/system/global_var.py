713c197cb144d70ebb23a8e0045db56b5c538811
Nguyễn Hoàng Dương <novakglow@gmail.com>
2017-09-30 16:11:39 +0700

    Put files in src folder

---
+++ src/system/global_var.py
@@ -0,0 +1,10 @@
+# System
+program_file_dir = "/usr/share/kaster"
+log_file_dir = program_file_dir + "/log.dat"
+date_format = "DD/MM/YYYY"
+
+# Strings for aid in random string generator
+uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_chars = uppercase_chars.lower()
+numbers = "0123456789"
+special_chars = "~`!@#$%^&*()-_+={}[]|\;:\"<>,./?"
