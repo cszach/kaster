1e6f15e7feb76476a8e81bb8444df74528ed2418
Nguyễn Hoàng Dương <novakglow@gmail.com>
2017-10-01 09:22:51 +0700

    Add new parameter to method write_to_log
    Add a parameter to specify whether to print the log message to the console or not

--- src/system/LogWriter.py
+++ src/system/LogWriter.py
@@ -9,19 +9,32 @@ class LogWriter(object):
     origin = os.getcwd()
 
     def create_log_file(self):
+        """
+        Create program's log file (log.dat) in /usr/share/kaster
+        :return:
+        """
         try:
             os.mkdir(global_var.program_file_dir)
             os.mknod(global_var.log_file_dir)
             with open(global_var.log_file_dir, "w") as f:
                 f.write("Log file created on %s." % (date(kaster.date_format)))
         except FileExistsError:
+            os.mknod(global_var.log_file_dir)
             return
         except Exception as e:
             print(e)
             sys.exit(1)
 
-    def write_to_log(self, log_str):
+    def write_to_log(self, log_str, print_log):
+        """
+        Write log message to log file
+        :param log_str: The log message to be written to the log file
+        :param print_log: Print log message to the console or not (boolean)
+        :return:
+        """
         try:
+            if print_log:
+                print(log_str)
             with open(global_var.log_file_dir, "a") as f:
                 f.write(("[%s %s] %s" + "\n") % (time(global_var.time_format), date(global_var.date_format), log_str))
         except IOError:
