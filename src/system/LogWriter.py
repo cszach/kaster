ed2d9fcebb1b07bd3561f59c840f2feddb543601
Nguyễn Hoàng Dương <novakglow@gmail.com>
2017-10-01 11:25:20 +0700

    Update log writer
    Add another switch for actions on IOError

--- src/system/LogWriter.py
+++ src/system/LogWriter.py
@@ -36,6 +36,9 @@ class LogWriter(object):
                 print(log_str)
             with open(global_var.log_file_dir, "a") as f:
                 f.write(("[%s %s] %s" + "\n") % (time(global_var.time_format), date(global_var.date_format), log_str))
-        except IOError:
-            self.create_log_file()
-            self.write_to_log(log_str, print_log)
+        except IOError as e:
+            if not os.path.isfile(global_var.log_file_dir):
+                self.create_log_file()
+                print("Error: Log file was not found. Outputs weren't saved.")
+            else:
+                print("Run the program as root")
