e9ff373a5639f27b1b670dca47efb1939f028147
Nguyễn Hoàng Dương <novakglow@gmail.com>
2017-10-13 20:15:53 +0700

    Use 'open' instead of 'mknod' to avoid errors

--- src/system/LogWriter.py
+++ src/system/LogWriter.py
@@ -14,12 +14,12 @@ def create_log_file():
     :return:
     """
     try:
-        if not os.path.isdir(global_var.program_file_dir):
-            os.mkdir(global_var.program_file_dir)
+        if not os.path.isdir(global_var.program_file_dir):  # Check program's directory existence
+            os.mkdir(global_var.program_file_dir)  # If the directory does not exist then create one
         if not os.path.isfile(global_var.log_file_dir):
             open(global_var.log_file_dir, 'a').close()  # Create empty log file
         with open(global_var.log_file_dir, "w") as f:
-            f.write("Log file created on %s." % (date(kaster.date_format)))
+            f.write("Log file created on %s.\n" % (date(global_var.date_format)))
         del f
     except Exception as e:
         print("An error has occurred. If permission is denied, try running the program as root.")
@@ -36,13 +36,13 @@ def write_to_log(log_str, print_log):
     :return:
     """
     try:
-        if print_log:
-            print(log_str)
         with open(global_var.log_file_dir, "a") as f:
             f.write(("[%s %s] %s" + "\n") % (time(global_var.time_format), date(global_var.date_format), log_str))
+        if print_log:
+            print(log_str)
     except IOError as e:
         if not os.path.isfile(global_var.log_file_dir):
-            self.create_log_file()
+            create_log_file()
             print("Error: Log file was not found. Outputs weren't saved.")
         else:
             print("Run the program as root")
