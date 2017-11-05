97f40671db71fa189d66d24171bf83259a671e0f
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-11-05 15:26:54 +0700

    Update LogWriter.py
    Make LogWriter its own main program

--- src/system/LogWriter.py
+++ src/system/LogWriter.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import global_var
-from k_date import date, time
+from k_date import k_now
+import Instructor
 
 
 # Kaster' log file manipulator, deals everything that involves the log file
@@ -17,9 +18,9 @@ def create_log_file():
         if not os.path.isdir(global_var.program_file_dir):  # Check program's directory existence
             os.mkdir(global_var.program_file_dir)  # If the directory does not exist then create one
         if not os.path.isfile(global_var.log_file_dir):
-            open(global_var.log_file_dir, 'a').close()  # Create empty log file
+            open(global_var.log_file_dir, "a").close()  # Create empty log file
         with open(global_var.log_file_dir, "w") as f:
-            f.write("Log file created on %s.\n" % (date(global_var.date_format)))
+            f.write("Log file created on %s\n" % k_now(global_var.time_format, global_var.date_format))
         del f
     except Exception as e:
         print("An error has occurred. If permission is denied, try running the program as root.")
@@ -28,24 +29,19 @@ def create_log_file():
         sys.exit(1)
 
 
-def write_to_log(log_str, print_log):
+def write_to_log(log_str):
     """
     Write log message to log file
     :param log_str: The log message to be written to the log file
-    :param print_log: Print log message to the console or not (boolean)
     :return:
     """
     try:
         with open(global_var.log_file_dir, "a") as f:
-            f.write(("[%s %s] %s" + "\n") % (time(global_var.time_format), date(global_var.date_format), log_str))
-        if print_log:
-            print(log_str)
-    except IOError as e:
+            f.write(("[%s] %s" + "\n") % (k_now(global_var.time_format, global_var.date_format), log_str))
+    except IOError:
         if not os.path.isfile(global_var.log_file_dir):
             create_log_file()
             print("Error: Log file was not found. Outputs weren't saved.")
-        else:
-            print("Run the program as root")
 
 
 def delete_log_file():
@@ -55,3 +51,46 @@ def delete_log_file():
     """
     if os.path.isfile(global_var.log_file_dir):
         os.remove(global_var.log_file_dir)
+
+
+def lw_main(com_list):
+    """
+    Main process of the log writer, called by the main program
+    :param com_list: Input arguments
+    :return:
+    """
+    if len(com_list) == 0:
+        Instructor.main("man_lw.txt")
+        sys.exit(0)
+    for l_idx, (l_opt, l_arg) in enumerate(com_list):
+        if l_opt == "--create":
+            create_log_file()
+        elif l_opt == "--append":
+            if os.path.isfile(global_var.log_file_dir):
+                with open(global_var.log_file_dir, "a") as f:
+                    f.write(l_arg + "\n")
+                del f
+            else:
+                if input("Log file does not exist, create one and write now? [Y|N] ").lower() == "y":
+                    create_log_file()
+                    lw_main([(l_opt, l_arg)] + com_list[l_idx + 1:])
+                    break
+        elif l_opt == "--log":
+            if os.path.isfile(global_var.log_file_dir):
+                write_to_log(l_arg)
+            else:
+                if input("Log file does not exist, create one and write now? [Y|N] ").lower() == "y":
+                    create_log_file()
+                    lw_main([(l_opt, l_arg)] + com_list[l_idx + 1:])
+                    break
+        elif l_opt == "--clear":
+            if os.path.isfile(global_var.log_file_dir):
+                os.remove(global_var.log_file_dir)
+                open(global_var.log_file_dir, "a").close()
+            else:
+                print("Log file does not exist")
+        elif l_opt == "--delete":
+            delete_log_file()
+        else:
+            print("Error: Not recognized option '%s'. Quitting..." % l_opt)
+            sys.exit(1)
