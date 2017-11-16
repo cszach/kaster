34ee5d871434806fc5eeeff3edc8ed34d10ecbf9
Novak Scottia <novakglow@gmail.com>
2017-11-16 20:21:35 +0700

    Resolve LogWriter's issue
    Getting ValueError while refreshing the file was still possible, because option --clear did not call create_log_file() function, but it deletes log.dat by itself, and just creates a new empty file called "log.dat".

--- src/system/pre_kaster.py
+++ src/system/pre_kaster.py
@@ -1,20 +1,10 @@
 # File for handling stuffs before the program actually start
 import sys
 import os
-import global_var
+from global_var import *
 from datetime import datetime
 import LogWriter
-
-
-def check_program_file_dir():
-    """
-    Check if the program's file directory exists (/usr/share/kaster)
-    If it does not, call create_log_file() function of the log writer,
-    which creates the program's file directory and then the log file in that directory
-    :return:
-    """
-    if not os.path.isdir(global_var.program_file_dir):
-        LogWriter.create_log_file()
+import traceback
 
 
 def renew_log_file():
@@ -23,8 +13,8 @@ def renew_log_file():
     :return:
     """
     # Get the date when the log file was created
-    if os.path.isfile(global_var.log_file_dir):
-        f = open(global_var.log_file_dir, "r")
+    if os.path.isfile(program_file_dir + "/log.dat"):
+        f = open(program_file_dir + "/log.dat", "r")
     else:
         return  # If no log file found then just return
     the_date = f.readline()  # Get the first line of the log file, which gives the time when the log file was created
@@ -36,12 +26,7 @@ def renew_log_file():
     # Compare and delete
     now = datetime.now()
     today = datetime(year=now.year, month=now.month, day=now.day)
-    try:
-        the_date = datetime(year=the_date[2], month=the_date[1], day=the_date[0])
-    except ValueError:
-        print("Warning: Failed to perform a start-up operation. Check the log file to see details.")
-        LogWriter.write_to_log("Fail to get the date on which the log file was created.")
-        return
+    the_date = datetime(year=the_date[2], month=the_date[1], day=the_date[0])
     if (today - the_date).days >= 30:  # Check if 30 days or more have passed since last time created log file
         LogWriter.delete_log_file()  # Remove the log file
         LogWriter.create_log_file()  # Create new, empty log file
@@ -52,5 +37,13 @@ def main():
     All processes to be ran on program's startup
     :return:
     """
-    check_program_file_dir()
-    renew_log_file()
+    # Create program's files path (/usr/share/kaster) if there isn't one
+    if not os.path.isdir(program_file_dir):
+        os.mkdir(program_file_dir)
+    try:
+        renew_log_file()
+    except ValueError:
+        print("An error occurred while the program was refreshing the log file.")
+        print("It's likely that the log file was somehow modified.")
+        print("=====Traceback=====")
+        traceback.print_exc()
