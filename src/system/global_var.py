39c7cfe72d0d4afadabe4e96a274531e76d62bcb
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-11-16 18:40:00 +0700

    Remove global variable 'log_file_dir'

--- src/system/global_var.py
+++ src/system/global_var.py
@@ -1,6 +1,5 @@
 # System
 program_file_dir = "/usr/share/kaster"
-log_file_dir = program_file_dir + "/log.dat"
 generator_file_dir = program_file_dir + "/gen"
 std_file_dir = program_file_dir + "/std"
 vault_file_dir = program_file_dir + "/vault"
