00c0013de093585087aab51617bdf1694e8623f3
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-12-04 06:50:07 +0700

    Implement long option '--edit'

--- src/kaster.py
+++ src/kaster.py
@@ -45,7 +45,7 @@ try:
                                 "std=",
                                 "length=", "duplicate=", "upper", "lower", "number", "symbol",
                                 "output=",
-                                "account", "new", "list", "get=", "getpass=", "del=", "delall",
+                                "account", "new", "list", "get=", "getpass=", "edit=", "del=", "delall",
                                 "name=", "login=", "password=", "comment="])
 except getopt.GetoptError as e:
     print("Error: ", end="")
