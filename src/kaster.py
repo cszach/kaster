9af00cba1441a8b51a8d5daaa987cd07acb8c3a3
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-12-03 12:37:22 +0700

    Add new long option '--get'

--- src/kaster.py
+++ src/kaster.py
@@ -45,7 +45,7 @@ try:
                                 "std=",
                                 "length=", "duplicate=", "upper", "lower", "number", "symbol",
                                 "output=",
-                                "account", "new"])
+                                "account", "new", "get="])
 except getopt.GetoptError as e:
     print("Error: ", end="")
     print(e)
