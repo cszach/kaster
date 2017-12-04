0a97fc3f7f452612d2f0df1229f0720baf5b28e0
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-12-04 20:17:38 +0700

    Update Instructor.py
    Catch keyboard interrupt

--- src/system/Instructor.py
+++ src/system/Instructor.py
@@ -1,21 +1,27 @@
+from sys import exit
+
+
 def main(man_page_name):
     """
     Main process for Instructor
     :return:
     """
-    if man_page_name is None:
-        # Read all the manual pages
-        print("Kaster Password Vault's help")
-        print()
-        man_pages = ["man_generic.txt", "man_std.txt", "man_lw.txt", "man_gen.txt"]
-        for man in man_pages:
-            main(man)
-            input()
-        del man, man_pages
-    else:
-        # Read specified manual page
-        f = open("manual/" + man_page_name, "r")
-        f_content = f.read()
-        f.close()
-        print(f_content)
-        del f, f_content
+    try:
+        if man_page_name is None:
+            # Read all the manual pages
+            print("Kaster Password Vault's help")
+            print()
+            man_pages = ["man_generic.txt", "man_std.txt", "man_lw.txt", "man_gen.txt", "man_vault.txt"]
+            for man in man_pages:
+                main(man)
+                input()
+            del man, man_pages
+        else:
+            # Read specified manual page
+            f = open("manual/" + man_page_name, "r")
+            f_content = f.read()
+            f.close()
+            print(f_content)
+            del f, f_content
+    except KeyboardInterrupt:
+        exit(0)
