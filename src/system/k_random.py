124684464598ca40b71a6956d428877f17b1a641
Novak Scottia <novakglow@gmail.com>
2017-12-18 00:58:27 +0700

    Implement random_pass function

--- src/system/k_random.py
+++ src/system/k_random.py
@@ -1,5 +1,7 @@
 from global_var import *
 from random import choice, randint
+from math import inf
+from k_check_pss import k_check_pss
 
 
 def random_string(*args):
@@ -70,10 +72,58 @@ def random_string(*args):
 
     # If our 4 booleans are set to None, pretend them to be set to True and allow all of them
     # because what password on Earth does not contain any of uppercase, lowercase, numerical and special characters?
-    if args[1] == args[2] == args[3] == args[4] == (False or None):
+    if args[1] == args[2] == args[3] == args[4] is None:
         allowed_chars = uppercase_chars + lowercase_chars + numbers + special_chars
 
     return "".join([choice(allowed_chars) for _ in range(pss_length)])
+
+
+def random_pass(std_filename):
+    """
+    Generate a random password that follows the specified @std standard
+    :param std_filename: Name of file defining the standard
+    :return: Randomly generated password
+    """
+    f = open("%s/%s" % (std_file_dir, std_filename))
+    f.readline()  # Bitch the first line
+    get_relative_score = eval(f.readline())
+    minimum_score = 5 if get_relative_score == 0 else 10
+    std_length = f.readline()
+    std_upper = f.readline()
+    std_lower = f.readline()
+    std_number = f.readline()
+    std_symbol = f.readline()
+    f.close()
+    del f
+    std_len_min = None
+    std_len_max = None
+    if std_length == ":":
+        std_len_min = 12
+        std_len_max = 30
+    elif std_length.split(":")[0].isdigit() and std_length.split(":")[1].isdigit():
+        std_len_min = int(std_length.split(":")[0])
+        std_len_max = int(std_length.split(":")[1])
+    elif std_length.split(":")[0].isdigit():
+        std_len_min = int(std_length.split(":")[0])
+        std_len_max = 30 if std_len_min < 30 else 45
+    elif std_length.split(":")[1].isdigit():
+        std_len_max = int(std_length.split(":")[1])
+        std_len_min = 12 if std_len_max > 12 else 8
+    del std_length
+
+    while True:
+        flag_return_password = random_string("ps", randint(std_len_min, std_len_max),
+                                             False if int(std_upper.split(":")[0]) == 0
+                                                      and int(std_upper.split(":")[1]) == 0 else True,
+                                             False if int(std_lower.split(":")[0]) == 0
+                                                      and int(std_lower.split(":")[1]) == 0 else True,
+                                             False if int(std_number.split(":")[0]) == 0
+                                                      and int(std_number.split(":")[1]) == 0 else True,
+                                             False if int(std_symbolmbol.split(":")[0]) == 0
+                                                      and int(std_symbol.split(":")[1]) == 0 else True,)
+        if k_check_pss(flag_return_password, std_filename) == minimum_score:
+            del std_len_min, std_len_max, std_upper, std_lower, std_number, std_symbol, minimum_score
+            return flag_return_password
 
 
 def random_hex(length):
