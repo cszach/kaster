e7b152e3c212743660e7f5948286346acbc6bdd4
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-12-19 22:30:44 +0700

    Update Instructor.py

--- src/system/Instructor.py
+++ src/system/Instructor.py
@@ -11,7 +11,7 @@ def main(man_page_name):
             # Read all the manual pages
             print("Kaster Password Vault's help")
             print()
-            man_pages = ["man_generic.txt", "man_std.txt", "man_lw.txt", "man_gen.txt", "man_vault.txt"]
+            man_pages = ["man_generic.txt", "man_lw.txt", "man_gen.txt", "man_vault.txt"]
             for man in man_pages:
                 main(man)
                 input()
