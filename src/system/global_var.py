aafbbd03e9563086940fd93a8449c6ab66b6b2b4
Novak Scottia <novakglow@gmail.com>
2017-12-20 22:45:38 +0700

    Remove std_file_dir

--- src/system/global_var.py
+++ src/system/global_var.py
@@ -1,7 +1,6 @@
 # System
 program_file_dir = "/usr/share/kaster"
 generator_file_dir = program_file_dir + "/gen"
-std_file_dir = program_file_dir + "/std"
 vault_file_dir = program_file_dir + "/vault"
 date_format = "%d/%m/%Y"
 time_format = "%H:%M:%S"
