a6ed9e3d021b60f0703a586714cb7aec3650b9b1
Novak Scottia <novakglow@gmail.com>
2017-12-24 22:02:43 +0700

    Create path.py

---
+++ src/utils/path.py
@@ -0,0 +1,21 @@
+from sys import exit
+import os.path
+
+
+def userhome():
+    """
+    Get (normal) user's home directory. Under sudo/root does not return '/root'
+    :return: User's home directory
+    """
+    if not os.path.isfile("../minion/grephome.sh"):
+        print("CRITICAL: Could not find grephome.sh: File missing")
+        exit(1)
+    os.system("chmod +x ../minion/grephome.sh")
+    os.system("bash ../minion/grephome.sh")
+    return open("/tmp/grephome.minion.product").read()[:-1]
+
+
+exec(open("%s/.kasterrc" % userhome()).read())
+log_path = "%s/%s" % (program_file_dir, "log.dat")
+man_f_dir = "%s/%s" % (program_file_dir, "man")
+vault_f_dir = "%s/%s" % (program_file_dir, "vault")
