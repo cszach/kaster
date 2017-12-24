0444de6bc98bfb404110dfa9b5acec939d793f4b
Novak Scottia <novakglow@gmail.com>
2017-12-24 17:45:47 +0700

    Update Instructor.py

--- src/system/Instructor.py
+++ src/system/Instructor.py
@@ -1,4 +1,5 @@
 from sys import exit
+import logging
 
 
 def main(man_page_name):
@@ -6,6 +7,13 @@ def main(man_page_name):
     Main process for Instructor
     :return:
     """
+    __process__ = "Instructor.py (main())"
+
+    logging.basicConfig(filename="%s" % program_file_dir + "/log.dat",
+                        format="[%(asctime)s] %(message)s",
+                        datefmt="%s %s" % (time_format, date_format),
+                        level=logging.INFO)
+
     try:
         if man_page_name is None:
             # Read all the manual pages
@@ -18,10 +26,15 @@ def main(man_page_name):
             del man, man_pages
         else:
             # Read specified manual page
-            f = open("manual/" + man_page_name, "r")
+            try:
+                f = open("manual/" + man_page_name, "r")
+            except FileNotFoundError:
+                logging.error("FATAL:%s: Couldn't find manual page (%s)" % (__process__, man_page_name))
+                exit(1)
             f_content = f.read()
             f.close()
             print(f_content)
             del f, f_content
     except KeyboardInterrupt:
+        logging.info("INFO:%s: Keyboard interrupted" % __process__)
         exit(0)
