985116220650e2f953f22e1fdd26ddc2437cd3ff
Novak Scottia <novakglow@gmail.com>
2017-12-26 06:46:20 +0700

    Update directory references

--- src/utils/k_path.py
+++ src/utils/k_path.py
@@ -5,11 +5,11 @@ import os.path
 # Get user's home directory
 # If user is under root or sudo then "/root" is not what we wanted
 # That's why the bash file is there to help
-if not os.path.isfile("../minion/grephome.sh"):
+if not os.path.isfile("minion/grephome.sh"):
     print("CRITICAL: Could not find grephome.sh: File missing")
     exit(1)
-os.system("chmod +x ../minion/grephome.sh")
-os.system("bash ../minion/grephome.sh")
+os.system("chmod +x minion/grephome.sh")
+os.system("bash minion/grephome.sh")
 
 userhome = open("/tmp/grephome.minion.product").read()[:-1]  # [:-1] to ignore the escape "\n"
 exec(open("%s/.kasterrc" % userhome).read())
