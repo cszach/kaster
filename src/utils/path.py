87705aea25139f89e6a2408f3c1666187b3a7ad3
Novak Scottia <novakglow@gmail.com>
2017-12-25 22:31:41 +0700

    Update path.py

--- src/utils/path.py
+++ src/utils/path.py
@@ -2,22 +2,20 @@ from sys import exit
 import os.path
 
 
-def userhome():
-    """
-    Get (normal) user's home directory. Under sudo/root does not return '/root'
-    :return: User's home directory
-    """
-    if not os.path.isfile("../minion/grephome.sh"):
-        print("CRITICAL: Could not find grephome.sh: File missing")
-        exit(1)
-    os.system("chmod +x ../minion/grephome.sh")
-    os.system("bash ../minion/grephome.sh")
-    return open("/tmp/grephome.minion.product").read()[:-1]
+# Get user's home directory
+# If user is under root or sudo then "/root" is not what we wanted
+# That's why the bash file is there to help
+if not os.path.isfile("../minion/grephome.sh"):
+    print("CRITICAL: Could not find grephome.sh: File missing")
+    exit(1)
+os.system("chmod +x ../minion/grephome.sh")
+os.system("bash ../minion/grephome.sh")
 
+userhome = open("/tmp/grephome.minion.product").read()[:-1]  # [:-1] to ignore the escape "\n"
+exec(open("%s/.kasterrc" % userhome).read())
 
-exec(open("%s/.kasterrc" % userhome()).read())
 kaster_dir = program_file_dir
-config_path = "%s/%s" % (userhome(), ".kasterrc")
+config_path = "%s/%s" % (userhome, ".kasterrc")
 log_path = "%s/%s" % (program_file_dir, "log.dat")
-man_f_dir = "%s/%s" % (program_file_dir, "man")
-vault_f_dir = "%s/%s" % (program_file_dir, "vault")
+man_dir = "%s/%s" % (program_file_dir, "man")
+vault_dir = "%s/%s" % (program_file_dir, "vault")
