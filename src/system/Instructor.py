b38e64fc311e81d1542baa1a0a083c5a97298f8f
Nguyễn Hoàng Dương <you_create@protonmail.com>
2018-03-31 20:26:55 +0700

    Update Instructor.py

--- src/system/Instructor.py
+++ src/system/Instructor.py
@@ -8,29 +8,25 @@ def main(man_page_name):
     Main process for Instructor
     :return:
     """
-    __process__ = "Instructor.py (main())"
+    __process__ = "Instructor.py -> main()"
 
-    try:
-        if man_page_name is None:
-            # Read all the manual pages
-            print("Kaster Password Vault's help")
-            print()
-            man_pages = ["man_generic.txt", "man_gen.txt", "man_vault.txt"]
-            for man in man_pages:
-                main(man)
-                input()
-            del man, man_pages
-        else:
-            # Read specified manual page
-            try:
-                f = open("manual/" + man_page_name, "r")
-            except FileNotFoundError:
-                kaster_logger.error("FATAL:%s: Couldn't find manual page (%s)" % (__process__, man_page_name))
-                sys.exit(1)
-            f_content = f.read()
-            f.close()
-            print(f_content)
-            del f, f_content
-    except KeyboardInterrupt:
-        kaster_logger.info("INFO:%s: Keyboard interrupted" % __process__)
-        exit(0)
+    if man_page_name is None:
+        # Read all the manual pages
+        print("Kaster Password Vault's help")
+        print()
+        man_pages = ["man_generic.txt", "man_gen.txt", "man_vault.txt"]
+        for man in man_pages:
+            main(man)
+            input()
+        del man, man_pages
+    else:
+        # Read specified manual page
+        try:
+            f = open("manual/" + man_page_name, "r")
+        except FileNotFoundError:
+            kaster_logger.error("ERROR::%s: Couldn't find manual page: %s" % (__process__, man_page_name))
+            return 1
+        f_content = f.read()
+        f.close()
+        print(f_content)
+        del f, f_content
