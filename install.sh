95d2a6610a6fd8409100c648e1afd70c395698b0
Nguyen Hoang Duong <you_create@protonmail.com>
2018-08-26 11:21:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,62 +1,78 @@
-#!/bin/bash
+#!/bin/sh
+# Install script for Kaster Password Vault
 
-if [ "$EUID" -ne 0 ]
+# ***** IMPORTANT VARIABLES *****
+# You may set these variables manually, but it's okay to leave them as default.
+
+kpv_version="Beta"  # Kaster version, must change for every Kaster's new release
+editor=""           # Path to text editor used to edit Kaster configuration file
+src_path="src"      # Kaster's source path, relative to this install script
+
+rcp="$src_path/system/.kasterrc"  # Path to .kasterrc in source
+kasp="$src_path/kaster.py"        # Path to kaster.py
+kaster_home="/usr/lib/kaster"     # Directory containing Kaster's program files
+userhome=$(eval echo ~$USER)      # Directory containing user's files
+
+# ********************************
+
+if ! [ $(id -u) == "0" ]
 then
-    echo "You need to run this file using bash as sudo/root."
-    exit 100
+    echo "Run this as root."
+    exit 1
 fi
 
-version="Beta"  # Must change for every Kaster's update
-echo "Installing Kaster password manager $version"
-unset version
+defc="\033[0m"; yellow="\033[1;33m"; red="\033[0;31m"  # Define colors
 
-# Get editor
-editor=""
-if [ -x "$(command -v nano)" ]
+echo -e "Installing Kaster Password Vault $kpv_version\n"
+
+exitcode="0"  # This script's exitcode
+
+# Get editor to edit .kasterrc
+if [ -x $(command -v nano) ] && [ -z $editor ]
 then
     editor="nano"
-elif [ -x "$(command -v vim)" ]
-then
-    editor="vim"
-elif [ -x "$(command -v vi)" ]
-then
-    editor="vi"
 else
-    echo "What text editor are you using? (enter the command that you can use to start the editor)"
-    read editor
-    if ! (( -x "$(command -v $editor)" ))
+    if [ -z $editor ]
     then
-        echo "FATAL: Command '$editor' does not available"
-        exit 407
+        read -p "What text editor do you prefer? (specify full path to executable)" editor
+    fi
+
+    if ! [ -x $editor ]
+    then
+        >&2 echo -e "${red}ERROR${defc}: $editor is not available"
+        editor=""
+        exitcode="4"
     fi
 fi
 
-userhome=$(eval echo ~$USER)
-
-if [ -e src/system/.kasterrc ]
+if [ -e $rcp ]
 then
-    echo "INFO: Opening $editor editor to edit src/system/.kasterrc (Kaster's configuration file)"
-    sleep 3
-    eval "$editor src/system/.kasterrc"
+    if [ -n $editor ]
+    then
+        echo "INFO: Opening $editor to configure Kaster"
+        sleep 3
+        eval "$editor $rcp"
 
-    echo "INFO: Moving src/system/.kasterrc to $userhome"
-    mv src/system/.kasterrc $userhome
-elif ! [ -e $userhome/.kasterrc ]
+        echo "INFO: Moving $rcp to $kaster_home"
+        mv $rcp $kaster_home
+    fi
+elif ! [ -e $kaster_home/.kasterrc ]
 then
-    echo "FATAL: Couldn't find .kasterrc in both current directory and home directory, file missing or has been moved"
-    exit 408
+    >&2 echo -e "${red}ERROR${defc}: Couldn't find .kasterrc, file missing or has been moved"
+    exitcode="5"
 else
-    echo "WARNING: Couldn't find .kasterrc in current directory but found one in home directory"
+    echo -e "${yellow}WARNING${defc}: Couldn't find .kasterrc in $rcp but found one in Kaster home ($kaster_home)"
     echo "It's possible that the .kasterrc in the home directory is of the earlier version of Kaster"
+    exitcode="2"
 fi
 
-if [ -e src/kaster.py ]
+if [ -e $kasp ]
 then
     echo "INFO: Making kaster.py executable"
-    chmod +x src/kaster.py
+    chmod +x $kasp
 else
-    echo "FATAL: Couldn't find kaster.py"
-    exit 400
+    >&2 echo -e "${red}ERROR${defc}: Couldn't find kaster.py"
+    exitcode="3"
 fi
 
 if [ -e undo_install.sh ]
@@ -64,10 +80,14 @@ then
     echo "INFO: Making undo_install.sh executable"
     chmod +x undo_install.sh
 else
-    echo "WARNING: Couldn't find undo_install.sh"
+    echo "${yellow}WARNING${defc}: Couldn't find undo_install.sh"
 fi
 
-echo "Done."
-unset userhome
-unset editor
-exit 0
+mv src/* $kaster_home
+
+if [ $exitcode -ge 4 ]
+then
+    >&2 echo -e "\nFAILED: Installation failed. Resolve errors (outputed to stderr) and try again."
+fi
+
+exit $exitcode
